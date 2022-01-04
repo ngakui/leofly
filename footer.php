a87c95c85f97b6679c493f1a99495b4edeee1442
seinokoula <alexandre.charles92@gmail.com>
2022-01-04 14:53:20 +0100

    :rocket: introduction of footer

--- footer.php
+++ footer.php
@@ -1,6 +1,16 @@
 <div id= "ttr_footer">
-<h1>FOOTER</h1>
-</div>
+    <div class="contain-footer">
+        <div class="text-footer">
+            <p>Contact</p>
+            <p>Mentions légales</p>
+            <p>Plan du site</p>
+        </div>
+        <div class="logo-footer">
+            <i class="fab fa-facebook-f"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-instagram"></i>
+        </div>
+    </div>
 </div>
 
 <?php wp_footer();?>
